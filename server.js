3c45645f210f1f199477d975a6755a49bba5adb9
Mateh Elismar (The New) <matematicoelismar@gmail.com>
2018-12-14 00:24:07 -0400

    he

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app = express()
 //Configurar el servidor
 app.set('port', process.env.PORT || 80)  
 
-app.set(express.static('alfredo. aqui va la url')) 
+app.set(express.static('alfredo. aqui va la direccion de la carpeta que contiene los archivos')) 
 //Hechar a correr el servidor
 app.listen(app.get('port'),()=>{
     console.log(`API RUNNING ON http://localhost:${app.get('port')}`)
