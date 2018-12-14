33f7cacdfd7ce207ca18c32a1afefd72af54e293
Mateh Elismar (The New) <matematicoelismar@gmail.com>
2018-12-14 00:21:34 -0400

    Primer commit

---
+++ server.js
@@ -0,0 +1,12 @@
+'use strict' 
+const express = require('express'), 
+app = express()
+
+//Configurar el servidor
+app.set('port', process.env.PORT || 80)  
+
+app.set(express.static('alfredo. aqui va la url')) 
+//Hechar a correr el servidor
+app.listen(app.get('port'),()=>{
+    console.log(`API RUNNING ON http://localhost:${app.get('port')}`)
+})
